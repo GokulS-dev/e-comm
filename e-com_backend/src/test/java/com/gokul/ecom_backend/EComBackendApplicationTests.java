d53ff611e33d915ede88a2f36179bc18fbe5850b
Gokul S <gokulsakthivel7004@gmail.com>
2026-06-23 18:04:16 +0530

    Replace FastAPI Backend with Spring Boot Backend Structure

---
+++ e-com_backend/src/test/java/com/gokul/ecom_backend/EComBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.ecom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
